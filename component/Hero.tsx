2a9f70b1037cd2d3517b87c95634a847d0ea5cb9
okoye micheal <okoyeikechukwu60@gmail.com>
2024-06-23 03:48:20 +0100

    first commit

---
+++ component/Hero.tsx
@@ -0,0 +1,88 @@
+import { Spotlight } from "@/component/ui/Spotlight";
+import { TextGenerateEffect } from "./ui/Text-Effect";
+import Shimmer from "./ui/Shimmer";
+import { FcCursor } from "react-icons/fc";
+import Link from "next/link";
+import Social from "./Social";
+import Photo from "./Picture";
+import { Vortex } from "./ui/Vortex";
+
+const Hero = () => {
+  return (
+    <Vortex
+      containerClassName="h-auto w-full pt-20 pb-36"
+      particleCount={700}
+      baseRadius={1}
+      baseHue={90}
+      rangeY={80}
+      rangeSpeed={1.5}
+      backgroundColor="#04052E"
+    >
+      <div className="">
+        {/**
+         *  UI: Spotlights
+         *  Link: https://ui.aceternity.com/components/spotlight
+         */}
+        <div>
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
+          />
+          <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="purple"
+          />
+          <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+        </div>
+
+        {/**
+         *  UI: grid
+         *  change bg color to bg-black-100 and reduce grid color from
+         *  0.2 to 0.03
+         */}
+
+        <div className="h-full flex justify-center z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[90vw] flex flex-col items-center justify-center xl:flex-row gap-10">
+            {/* text */}
+
+            <div className="text-center xl:text-left lg:max-w-[40vw]">
+              <span>Hello i&apos;m</span>
+              <h1 className="text-[40px] font-bold capitalize leading-[3rem]">
+                rachid <br />
+                <span className="under-line">Amgoun</span>
+              </h1>
+              <div className="py-8">
+                <p>
+                  I have the perfect solution for your web development needs. By
+                  adopting a modern stack, like MERN, and incorporating
+                  TypeScript for enhanced type safety, we can create an
+                  exceptional web application.
+                </p>
+              </div>
+              <div className="mt-4 flex flex-col items-center justify-center xl:flex-row xl:justify-start xl:space-x-8">
+                <div className="flex gap-x-2">
+                  <Link
+                    href="https://res.cloudinary.com/dnsteuqq1/image/upload/cv_blijgd.pdf"
+                    target="_blank"
+                    className="flex items-center space-x-2 rounded-full border-2 border-blue px-6 py-3 font-medium uppercase text-blue transition-all hover:bg-blue/80 hover:text-lighted"
+                  >
+                    <span className="text-sm">Download Cv</span>
+                    {/* <Download className="size-5" /> */}
+                  </Link>
+                </div>
+                <div className="flex py-4 xl:py-0">
+                  <Social />
+                </div>
+              </div>
+            </div>
+            {/* image */}
+
+            <Photo color="#6E8898" />
+          </div>
+        </div>
+      </div>
+    </Vortex>
+  );
+};
+
+export default Hero;
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://1439ee3e54a97c3140d137ec88e36e57@o4507459463479296.ingest.de.sentry.io/4507460639195216",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
